dfdbd9644e275af9cea0bb4000616c7ff6c6ddf2
Lachicagradiadora <200379@unsaac.edu.pe>
2023-04-21 22:54:52 -0500

    problem with dom notes and bookmark and add buttons

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ import './style.css'
 // SEARCHER
 
 const containInputElement=document.getElementById('containSInput')
-const containButtonElement=document.getElementById('containSButton')
+const containButtonElement=document.getElementById('containSearchButton')
 
 
 const roundedInput=({id,type,placeholder,name})=>{
@@ -41,16 +41,15 @@ const roundedInput=({id,type,placeholder,name})=>{
 containInputElement.innerHTML=roundedInput({id:'searchInput',type:'search',placeholder:'write note',name:'searchInput'})
 const searchInputElement=document.getElementById('searchInput')
 searchInputElement.classList.add('search-input')
-// searchInputElement.style.background=red;
 
-const roundedButton=({id,children,onclick})=>{
+const roundedButton=({id,children,onclick,type})=>{
   return`
-  <button id='${id}'>
+  <button id='${id}' type='${type}' onclick,>
     ${children}
   </button>
   `
-};
-containButtonElement.innerHTML=roundedButton({id:'searchButton',onclick, children:'<i id="img" class="fa fa-search" aria-hidden="true"></i>'})
+}
+containButtonElement.innerHTML=roundedButton({id:'searchButton',type:'submit', children:'<i id="img" class="fa fa-search" aria-hidden="true"></i>'})
 const searchButtonElement=document.getElementById('searchButton')
 searchButtonElement.classList.add('search-button')
 
@@ -67,12 +66,44 @@ containNotesElement.innerHTML=itemNotesList
 const noteItemElement=document.getElementById('noteItem')
 
 
+//BUTTON FAVORITES AND ADD
+
+// BUTTON OF FAVORITES
+const favoritesElement=document.getElementById('favorites')
+
+const buttonFavorites=({id, children})=>{
+  return`
+  <button id='${id}'>
+  ${children}
+  </button>
+  `
+}
+favoritesElement.innerHTML=buttonFavorites({id:'buttonFavorites', children:'<i class="fa fa-star" aria-hidden="true"></i>'})
+const buttonFavoritesElement=document.getElementById('buttonFavorites')
+buttonFavoritesElement.classList.add('button-favorites')
+
+// BUTTON FOR ADD NOTE
+const newNoteElement=document.getElementById('newNote')
+
+const buttonAddNotes=({id,children})=>{
+  return`
+  <button id='${id}'>
+  ${children}
+  </button>
+  `
+}
+newNoteElement.innerHTML=buttonAddNotes({id:'buttonAddNotes',children:'<i class="fa fa-plus" aria-hidden="true"></i>'})
+const buttonAddNotesElement=document.getElementById('buttonAddNotes')
+buttonAddNotesElement.classList.add('add-notes')
+
+
 //save notes in local storage
 
 //responsive
 
 
 //add favorites 
+
 
 
 //add favorites 
